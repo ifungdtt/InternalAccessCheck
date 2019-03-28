e030738dc0f7c325ec20c7170c294b3fe0fc4572
sleungdtt <sleung@deloitte.com.hk>
2019-03-28 12:30:38 +0800

    initial commit

---
+++ InternalAccessCheck/InternalAccessCheck/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InternalAccessCheck
+{
+    public partial class MainPage : ContentPage
+    {
+        private string TAG = "INTERNAL_ACCESS_CHECK";
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Console.WriteLine(TAG + ": Button_Clicked");
+                RestService restService = new RestService();
+                if (await restService.IsReachable(Entry.Text))
+                {
+                    Label.Text = Entry.Text + " is reachable";
+                    Console.WriteLine(TAG + ": "+ Entry.Text + " is reachable");
+                }
+                else
+                {
+                    Label.Text = Entry.Text + " is not reachable";
+                    Console.WriteLine(TAG + ": " + Entry.Text + " is not reachable");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(TAG + ":" + ex.Message + ", " + ex.StackTrace);
+            }
+            
+        }
+    }
+}
---
+++ InternalAccessCheck/InternalAccessCheck/RestService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+
+namespace InternalAccessCheck
+{
+    public class RestService
+    {
+        private string TAG = "INTERNAL_ACCESS_CHECK";
+        HttpClient client;
+
+
+        public RestService()
+        {
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+        }
+
+        public async Task<bool> IsReachable(string target)
+        {
+            try
+            {
+
+                Console.WriteLine(TAG + ":IsReachable");
+                var connectivity = CrossConnectivity.Current;
+                if (!connectivity.IsConnected)
+                {
+                    Console.WriteLine(TAG + ":!connectivity.IsConnected");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine(TAG + ":connectivity.IsConnected");
+                }
+
+                var reachable = await connectivity.IsRemoteReachable(target);
+                Console.WriteLine(TAG + ":reachable - "+reachable);
+                return reachable;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(TAG + ":" + ex.Message + ", " + ex.StackTrace);
+                return false;
+            }
+        }
+
+    }
+}
